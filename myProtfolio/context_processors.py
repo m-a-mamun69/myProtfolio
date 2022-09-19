bfc736a59ed0a4a8a81eb5833e5eef4ce9d0d0ae
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2022-09-19 18:45:38 +0600

    My Protfolio Project commited

---
+++ myProtfolio/context_processors.py
@@ -0,0 +1,13 @@
+from multiprocessing import context
+from django.contrib.auth.models import User
+
+def project_context(request):
+    context = {
+        'me': User.objects.first(),
+    }
+
+    return context
+
+
+
+    
